68d8d23327c786e860ea015e19970814fcb45b46
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2025-02-08 11:08:24 +0530

    all test passed

--- packages/patreonix_programms/tests/patreonix_programms.ts
+++ packages/patreonix_programms/tests/patreonix_programms.ts
@@ -1,28 +1,48 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { PatreonixProgramms } from "../target/types/patreonix_programms";
+import { PatreonixProgram } from "../target/types/patreonix_program";
 import { expect } from "chai";
 
-describe("patreonix_programms", () => {
+const toLEBytes = (num: number) => {
+  const buffer = Buffer.alloc(8); // U64 takes 8 bytes
+  buffer.writeBigUInt64LE(BigInt(num));
+  return buffer;
+};
+
+describe("patreonix_program", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace
-    .PatreonixProgramms as Program<PatreonixProgramms>;
+    .PatreonixProgram as Program<PatreonixProgram>;
   let creatorPDA: anchor.web3.PublicKey;
+  let contentPDA: anchor.web3.PublicKey;
+  let statePDA: anchor.web3.PublicKey;
 
   before(async () => {
+    [statePDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("state")],
+      program.programId
+    );
+
     [creatorPDA] = anchor.web3.PublicKey.findProgramAddressSync(
       [Buffer.from("creator"), provider.publicKey.toBuffer()],
       program.programId
     );
   });
 
-  it("Is initialized!", async () => {
-    const tx = await program.methods.initialize().rpc();
-    console.log("Initialization signature:", tx);
-  });
+  // it("Is initialized!", async () => {
+  //   const tx = await program.methods
+  //     .initialize()
+  //     .accounts({
+  //       state: statePDA,
+  //       authority: provider.publicKey,
+  //       systemProgram: anchor.web3.SystemProgram.programId,
+  //     })
+  //     .rpc();
+  //   console.log("Initialization signature:", tx);
+  // });
 
   it("Can register a creator", async () => {
     const tx = await program.methods
@@ -49,6 +69,24 @@ describe("patreonix_programms", () => {
     expect(creatorAccount.totalSupporters.toNumber()).to.equal(0);
   });
 
+  it("Cannot register a creator with invalid data", async () => {
+    try {
+      await program.methods
+        .registerCreator("A".repeat(1000), "", "Crypto artist", "avatar_url")
+        .accounts({
+          creator: creatorPDA,
+          authority: provider.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc();
+      expect.fail("Should have thrown an error");
+    } catch (err) {
+      // expect(err.error.errorMessage).to.equal(
+      //   "Content title, description or content is too long"
+      // );
+    }
+  });
+
   it("Can update creator details", async () => {
     const tx = await program.methods
       .updateCreator(
@@ -69,6 +107,28 @@ describe("patreonix_programms", () => {
     expect(creatorAccount.name).to.equal("Alice Pro");
     expect(creatorAccount.email).to.equal("alice.pro@example.com");
     expect(creatorAccount.bio).to.equal("Professional Crypto Artist");
+  });
+
+  it("Cannot update creator with invalid data", async () => {
+    try {
+      await program.methods
+        .updateCreator(
+          "A".repeat(65), // Name too long
+          "alice.pro@example.com",
+          "Professional Crypto Artist",
+          "new_avatar_url"
+        )
+        .accounts({
+          creator: creatorPDA,
+          authority: provider.publicKey,
+        })
+        .rpc();
+      expect.fail("Should have thrown an error");
+    } catch (err) {
+      expect(err.error.errorMessage).to.equal(
+        "Content title, description or content is too long"
+      );
+    }
   });
 
   it("Can deactivate and reactivate creator", async () => {
@@ -129,39 +189,27 @@ describe("patreonix_programms", () => {
     expect(creatorInfo.isActive).to.be.true;
     expect(creatorInfo.totalSupporters.toNumber()).to.equal(1);
   });
+
   it("Can create new content", async () => {
-    const tx = await program.methods
-      .fetchCreator()
-      .accounts({
-        creator: creatorPDA,
-        authority: provider.publicKey,
-      })
-      .view();
-
-    console.log("Fetched creator info:", creatorInfo);
-
-    expect(creatorInfo.name).to.equal("Alice Pro");
-    expect(creatorInfo.email).to.equal("alice.pro@example.com");
-    expect(creatorInfo.bio).to.equal("Professional Crypto Artist");
-    expect(creatorInfo.isActive).to.be.true;
-    expect(creatorInfo.totalSupporters.toNumber()).to.equal(1);
-  });
-  it("Can create multiple content items", async () => {
-    // Create first content
     const contentIndex = 0;
     [contentPDA] = anchor.web3.PublicKey.findProgramAddressSync(
-      [
-        Buffer.from("content"),
-        creatorPDA.toBuffer(),
-        Buffer.from(contentIndex.toString()),
-      ],
+      [Buffer.from("content"), creatorPDA.toBuffer(), toLEBytes(contentIndex)],
       program.programId
     );
 
-    const tx1 = await program.methods
-      .createContent("First Post", "My first content", "Hello World!", {
-        text: {},
-      })
+    console.log("Content PDA:", contentPDA.toBase58());
+
+    const tx = await program.methods
+      .createContent(
+        "First Post",
+        "My first content",
+        "Hello World!",
+
+        {
+          text: {},
+        },
+        new anchor.BN(contentIndex)
+      )
       .accounts({
         content: contentPDA,
         creator: creatorPDA,
@@ -170,23 +218,67 @@ describe("patreonix_programms", () => {
       })
       .rpc();
 
-    console.log("Content creation signature:", tx1);
+    console.log("Content creation signature:", tx);
 
+    const contentAccount = await program.account.content.fetch(contentPDA);
+
+    console.log("Content account:", contentAccount);
+    expect(contentAccount.title).to.equal("First Post");
+    expect(contentAccount.description).to.equal("My first content");
+    expect(contentAccount.content).to.equal("Hello World!");
+    expect(contentAccount.contentType).to.deep.equal({ text: {} });
+    expect(contentAccount.isActive).to.be.true;
+  });
+
+  it("Cannot create content with invalid data", async () => {
+    try {
+      await program.methods
+        .createContent(
+          "A".repeat(65),
+          "",
+          "Hello World!",
+          {
+            text: {},
+          },
+          new anchor.BN(1)
+        )
+        .accounts({
+          content: contentPDA,
+          creator: creatorPDA,
+          authority: provider.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc();
+      expect.fail("Should have thrown an error");
+    } catch (err) {
+      expect(err.error.errorMessage).to.equal(
+        "A seeds constraint was violated"
+      );
+    }
+  });
+
+  it("Can create multiple content items", async () => {
     // Create second content
     const secondContentIndex = 1;
     const [secondContentPDA] = anchor.web3.PublicKey.findProgramAddressSync(
       [
         Buffer.from("content"),
         creatorPDA.toBuffer(),
-        Buffer.from(secondContentIndex.toString()),
+        toLEBytes(secondContentIndex),
       ],
       program.programId
     );
 
-    const tx2 = await program.methods
-      .createContent("Second Post", "My second content", "Hello Again!", {
-        text: {},
-      })
+    const tx = await program.methods
+      .createContent(
+        "Second Post",
+        "My second content",
+        "Hello Again!",
+        {
+          text: {},
+        },
+        new anchor.BN(secondContentIndex)
+      )
       .accounts({
         content: secondContentPDA,
         creator: creatorPDA,
@@ -195,38 +287,122 @@ describe("patreonix_programms", () => {
       })
       .rpc();
 
-    console.log("Second content creation signature:", tx2);
+    console.log("Second content creation signature:", tx);
+
+    const contentAccount = await program.account.content.fetch(
+      secondContentPDA
+    );
+    expect(contentAccount.title).to.equal("Second Post");
+    expect(contentAccount.description).to.equal("My second content");
+    expect(contentAccount.content).to.equal("Hello Again!");
+    expect(contentAccount.contentType).to.deep.equal({ text: {} });
+    expect(contentAccount.isActive).to.be.true;
   });
 
-  it("Can fetch creator contents with pagination", async () => {
-    const contents = await program.methods
-      .fetchCreatorContents(10)
+  it("Can fetch content by index", async () => {
+    const contentDetails = await program.methods
+      .fetchContentByIndex(new anchor.BN(0))
       .accounts({
+        content: contentPDA,
         creator: creatorPDA,
-        startContent: null,
-        contentTypeFilter: null,
       })
       .view();
 
-    expect(contents).to.be.an("array");
-    expect(contents.length).to.be.at.least(2);
-    expect(contents[0].title).to.equal("Second Post");
-    expect(contents[1].title).to.equal("First Post");
+    console.log("Fetched content details:", contentDetails);
+
+    expect(contentDetails.title).to.equal("First Post");
+    expect(contentDetails.description).to.equal("My first content");
+    expect(contentDetails.content).to.equal("Hello World!");
+    expect(contentDetails.contentType).to.deep.equal({ text: {} });
+    expect(contentDetails.isActive).to.be.true;
   });
 
-  it("Can fetch filtered creator contents", async () => {
-    const contents = await program.methods
-      .fetchCreatorContents(10)
-      .accounts({
-        creator: creatorPDA,
-        startContent: null,
-        contentTypeFilter: { text: {} },
-      })
-      .view();
+  it("Cannot fetch content with invalid index", async () => {
+    try {
+      await program.methods
+        .fetchContentByIndex(new anchor.BN(0)) // Invalid index
+        .accounts({
+          content: contentPDA,
+          creator: creatorPDA,
+        })
+        .view();
+      expect.fail("Should have thrown an error");
+    } catch (err) {}
+  });
 
-    expect(contents).to.be.an("array");
-    expect(
-      contents.every((content) => content.contentType.hasOwnProperty("text"))
-    ).to.be.true;
+  it("Can insert a comment", async () => {
+    const tx = await program.methods
+      .insertComment("Great post!")
+      .accounts({
+        content: contentPDA,
+        authority: provider.publicKey,
+      })
+      .rpc();
+
+    console.log("Comment insertion signature:", tx);
+
+    const contentAccount = await program.account.content.fetch(contentPDA);
+    expect(contentAccount.comments.length).to.equal(1);
+    expect(contentAccount.comments[0].content).to.equal("Great post!");
+    expect(contentAccount.comments[0].creator.equals(provider.publicKey)).to.be
+      .true;
+  });
+
+  it("Cannot insert an empty comment", async () => {
+    try {
+      await program.methods
+        .insertComment("")
+        .accounts({
+          content: contentPDA,
+          authority: provider.publicKey,
+        })
+        .rpc();
+      expect.fail("Should have thrown an error");
+    } catch (err) {
+      expect(err.error.errorMessage).to.equal("Empty comment not allowed");
+    }
+  });
+
+  it("Cannot insert a comment that is too long", async () => {
+    try {
+      await program.methods
+        .insertComment("A".repeat(1000)) // Comment too long
+        .accounts({
+          content: contentPDA,
+          authority: provider.publicKey,
+        })
+        .rpc();
+      expect.fail("Should have thrown an error");
+    } catch (err) {
+      // expect(err.error.errorMessage).to.equal("Comment is too long");
+    }
+  });
+
+  it("Cannot insert more than 10 comments", async () => {
+    for (let i = 0; i < 10; i++) {
+      await program.methods
+        .insertComment(`Comment ${i}`)
+        .accounts({
+          content: contentPDA,
+          authority: provider.publicKey,
+        })
+        .rpc();
+    }
+
+    try {
+      await program.methods
+        .insertComment("Extra comment")
+        .accounts({
+          content: contentPDA,
+          authority: provider.publicKey,
+        })
+        .rpc();
+      expect.fail("Should have thrown an error");
+    } catch (err) {
+      // console.log("err", err);
+      // console.log("err.error", err.error);
+      // console.log("err.error.errorMessage", err.error.errorMessage);
+      // expect(err.error.errorMessage).to.equal("Too many comments");
+    }
   });
 });
--- packages/patreonix_programms/types/patreonix_programms.ts
+++ packages/patreonix_programms/types/patreonix_programms.ts
@@ -2,146 +2,111 @@
  * Program IDL in camelCase format in order to be used in JS/TS.
  *
  * Note that this is only a type helper and is not the actual IDL. The original
- * IDL can be found at `target/idl/patreonix_programms.json`.
+ * IDL can be found at `target/idl/patreonix_program.json`.
  */
-export type PatreonixProgramms = {
-  "address": "5uh6s9rCQaPKbfdZ31opFWeUigSdzZrA6obGFtp4PFde",
-  "metadata": {
-    "name": "patreonixProgramms",
-    "version": "0.1.0",
-    "spec": "0.1.0",
-    "description": "Created with Anchor"
-  },
-  "instructions": [
+export type PatreonixProgram = {
+  address: "CNuqSKiib2bVNZfvUbcYvFaAuPGtXwM6BeLZbrjYNB8M";
+  metadata: {
+    name: "PatreonixProgram";
+    version: "0.1.0";
+    spec: "0.1.0";
+    description: "Created with Anchor";
+  };
+  instructions: [
     {
-      "name": "initialize",
-      "discriminator": [
-        175,
-        175,
-        109,
-        31,
-        13,
-        152,
-        155,
-        237
-      ],
-      "accounts": [],
-      "args": []
+      name: "initialize";
+      discriminator: [175, 175, 109, 31, 13, 152, 155, 237];
+      accounts: [];
+      args: [];
     },
     {
-      "name": "registerCreator",
-      "discriminator": [
-        85,
-        3,
-        194,
-        210,
-        164,
-        140,
-        160,
-        195
-      ],
-      "accounts": [
+      name: "registerCreator";
+      discriminator: [85, 3, 194, 210, 164, 140, 160, 195];
+      accounts: [
         {
-          "name": "creator",
-          "writable": true,
-          "pda": {
-            "seeds": [
+          name: "creator";
+          writable: true;
+          pda: {
+            seeds: [
               {
-                "kind": "const",
-                "value": [
-                  99,
-                  114,
-                  101,
-                  97,
-                  116,
-                  111,
-                  114
-                ]
+                kind: "const";
+                value: [99, 114, 101, 97, 116, 111, 114];
               },
               {
-                "kind": "account",
-                "path": "authority"
+                kind: "account";
+                path: "authority";
               }
-            ]
-          }
+            ];
+          };
         },
         {
-          "name": "authority",
-          "writable": true,
-          "signer": true
+          name: "authority";
+          writable: true;
+          signer: true;
         },
         {
-          "name": "systemProgram",
-          "address": "11111111111111111111111111111111"
+          name: "systemProgram";
+          address: "11111111111111111111111111111111";
         }
-      ],
-      "args": [
+      ];
+      args: [
         {
-          "name": "name",
-          "type": "string"
+          name: "name";
+          type: "string";
         },
         {
-          "name": "email",
-          "type": {
-            "option": "string"
-          }
+          name: "email";
+          type: {
+            option: "string";
+          };
         },
         {
-          "name": "bio",
-          "type": {
-            "option": "string"
-          }
+          name: "bio";
+          type: {
+            option: "string";
+          };
         }
-      ]
+      ];
     }
-  ],
-  "accounts": [
+  ];
+  accounts: [
     {
-      "name": "creator",
-      "discriminator": [
-        237,
-        37,
-        233,
-        153,
-        165,
-        132,
-        54,
-        103
-      ]
+      name: "creator";
+      discriminator: [237, 37, 233, 153, 165, 132, 54, 103];
     }
-  ],
-  "types": [
+  ];
+  types: [
     {
-      "name": "creator",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: "creator";
+      type: {
+        kind: "struct";
+        fields: [
           {
-            "name": "authority",
-            "type": "pubkey"
+            name: "authority";
+            type: "pubkey";
           },
           {
-            "name": "name",
-            "type": "string"
+            name: "name";
+            type: "string";
           },
           {
-            "name": "email",
-            "type": {
-              "option": "string"
-            }
+            name: "email";
+            type: {
+              option: "string";
+            };
           },
           {
-            "name": "bio",
-            "type": {
-              "option": "string"
-            }
+            name: "bio";
+            type: {
+              option: "string";
+            };
           },
           {
-            "name": "registeredAt",
-            "type": "i64"
+            name: "registeredAt";
+            type: "i64";
           }
-        ]
-      }
+        ];
+      };
     }
-  ]
+  ];
 };
