c28d79a61040e079ce509b13a29bcdc0d447b3f9
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2025-02-09 19:02:23 +0530

    usePatreonix hook added

--- apps/creator/src/hooks/usePatreonix.ts
+++ apps/creator/src/hooks/usePatreonix.ts
@@ -1,65 +1,30 @@
+import {
+  AnchorWallet,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
 import { useMemo } from "react";
-import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
-import { Program, AnchorProvider, setProvider } from "@coral-xyz/anchor";
-import { PublicKey } from "@solana/web3.js";
-import idl from "@repo/patreonix_programms/idl";
-import { PatreonixProgramms } from "@repo/patreonix_programms/types";
+import * as anchor from "@coral-xyz/anchor";
+import { Patreonix } from "@/lib/solana";
 
-interface ProgramHookState {
-  program: Program<PatreonixProgramms> | null;
-  provider: AnchorProvider | null;
-  error: Error | null;
-  loading: boolean;
-  connected: boolean;
-  PROGRAM_ID: PublicKey;
-}
-
-const PROGRAM_ID = new PublicKey(idl.address);
-
-export function useCounterProgram(): ProgramHookState {
+export function useProgram() {
   const { connection } = useConnection();
-  const wallet = useAnchorWallet();
 
-  return useMemo((): ProgramHookState => {
-    if (!wallet) {
-      return {
-        program: null,
-        provider: null,
-        error: null,
-        loading: false,
-        connected: false,
-        PROGRAM_ID,
-      };
-    }
+  const wallet = useWallet();
 
-    try {
-      const provider = new AnchorProvider(connection, wallet, {
+  const program = useMemo(() => {
+    if (!wallet.publicKey) return null;
+
+    const provider = new anchor.AnchorProvider(
+      connection,
+      wallet as AnchorWallet,
+      {
         commitment: "confirmed",
-      });
-      setProvider(provider);
+      }
+    ); //provider
 
-      const program = new Program(idl as PatreonixProgramms, provider);
-
-      return {
-        program,
-        provider,
-        error: null,
-        loading: false,
-        connected: true,
-        PROGRAM_ID,
-      };
-    } catch (error) {
-      return {
-        program: null,
-        provider: null,
-        error:
-          error instanceof Error
-            ? error
-            : new Error("Failed to initialize program"),
-        loading: false,
-        connected: false,
-        PROGRAM_ID,
-      };
-    }
+    return new Patreonix(provider,true); //instance
   }, [connection, wallet]);
+
+  return { program };
 }
